8eb9f9c5f88327e5fce70834efa6ef47158cdc57
ariel- <ariel-@users.noreply.github.com>
2018-04-13 03:41:15 -0300

    Added TP #2

---
+++ 02-EjemploTipos/EjemploTipos.cpp
@@ -0,0 +1,82 @@
+
+/*
+ * EjemploTipos.cpp
+ * Ejemplificación de tipos de datos
+ * Ariel Silva
+ * 20180413
+ */
+
+#include <cassert>
+#include <ciso646>
+#include <cmath>
+#include <string>
+
+int main()
+{
+    // bool
+    bool interruptor{ false };
+    interruptor = interruptor and true;
+    assert(interruptor == false);
+
+    interruptor = interruptor or true;
+    assert(interruptor == true);
+
+    interruptor = not interruptor;
+    assert(interruptor == false);
+
+    // char
+    char alpha{ 64 };
+    assert(alpha == '@');
+
+    alpha = 'a' + 5;
+    assert(alpha == 'f');
+
+    alpha = '0' + 7;
+    assert(alpha == '7');
+
+    alpha = 'z' - 'a' + 1;
+    assert(alpha == 26);
+
+    // unsigned
+    unsigned natural{ 9899984 };
+    natural = natural % 3;
+    assert(natural == 2);
+
+    natural = 16 * 8;
+    assert(natural == 128);
+
+    natural = natural - 120;
+    assert(natural == 8);
+
+    // int
+    int entero{ -88 };
+    entero = entero + 128;
+    assert(entero == 40);
+
+    entero = entero / natural;
+    assert(entero == 5);
+
+    // double
+    double pi{ 4.0 * std::atan(1.0) };
+    assert(std::fabs(pi - 3.14159265) <= 1e-5);
+
+    double raizdos{ std::sqrt(2.0) };
+    assert(std::fabs(raizdos - 1.41421356) <= 1e-5);
+
+    raizdos = raizdos / 2.0;
+    assert(std::fabs(raizdos - 0.70710678) <= 1e-5);
+
+    double seno = std::sin(pi);
+    assert(std::fabs(seno - 0.0) <= 1e-5);
+
+    double coseno = std::cos(pi / 4.0);
+    assert(std::fabs(coseno - raizdos) <= 1e-5);
+
+    // string
+    std::string hola{ "Hola " };
+    assert(hola.length() == 5);
+
+    hola = hola + "Mundo";
+    assert(hola.length() == 10);
+    assert(hola == "Hola Mundo");
+}
---
+++ 02-EjemploTipos/readme.md
@@ -0,0 +1,38 @@
+# TP #2 - Ejemplos de Valores y Operaciones de Tipos de Datos
+
+## Autores
+Usuario | Legajo | Apellido | Nombre
+------- | ------ | -------- | ------
+[ariel-](https://github.com/ariel-) | 166216-8 | Silva | Ariel
+
+## Enunciado
+### Objetivos
+* Mediante un ejemplo, demostrar la aplicación de tipos de datos.
+
+### Temas
+* Tipos de datos.
+* Declaraciones.
+* Variables.
+* Valores.
+
+### Problema
+Diseñar un programa C++ que ejemplifique la aplicación de los tipos de datos vistos en clases.
+
+### Tareas
+* Este es un _trabajo no estructurado_, que consiste en escribir un programa que ejemplifique el uso de los tipos de datos básicos de C++ vistos en clase: `bool`, `char`, `unsigned`, `int`, `double`, y `string`.
+
+#### Crédito Extra
+* ¿Son esos realmente todos los tipos que vimos en clase? Justifique.
+* No utilice `cout` y sí utilice `assert` para las pruebas.
+
+### Restricciones
+* No extraer valores de `cin`, usar valores literales (constantes).
+
+### Productos
+* readme.md
+* EjemploTipos.cpp
+
+## Resolución
+### Observaciones
+* Al ser imposible la igualdad exacta del tipo `double`, se utiliza una tolerancia de 0.00001 para compararlos
+* Si bien no se utilizan en este ejemplo, en trabajos anteriores se utilizaron las variables globales `cin` y `cout`, de tipo `istream` y `ostream` respectivamente, entre otras operaciones poseen `>>` y `<<` respectivamente.
